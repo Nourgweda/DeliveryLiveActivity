49494e40b6fbc430f000e2ee8f54f0e08774c648
trianglz <nour.gweda@trianglz.com>
2022-12-04 12:12:06 +0200

    Initial Commit

---
+++ DeliveryLiveActivity/DeliveryLiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryLiveActivityApp.swift
+//  DeliveryLiveActivity
+//
+//  Created by Nour Gweda on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryLiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
